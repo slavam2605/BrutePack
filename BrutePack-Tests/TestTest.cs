34263829e4d9155582c93bdf90c1fd4c04b9bcbd
Nikolay Kuznetsov <kna.rus@gmail.com>
2016-09-04 01:47:49 +0300

    Added project that will contain the tests

---
+++ BrutePack-Tests/TestTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BrutePack_Tests
+{
+    [TestFixture]
+    public class TestTest
+    {
+        [Test]
+        public void RunSimpleTest()
+        {
+            Console.WriteLine("Test running");
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
