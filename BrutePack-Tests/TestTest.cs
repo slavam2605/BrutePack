dfdc897e58e6de34b140cd6c76ee35da7fb1a263
Nikolay Kuznetsov <kna.rus@gmail.com>
2016-09-05 20:22:33 +0300

    Added test directory util

--- BrutePack-Tests/TestTest.cs
+++ BrutePack-Tests/TestTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace BrutePack_Tests
@@ -9,6 +10,7 @@ namespace BrutePack_Tests
         [Test]
         public void RunSimpleTest()
         {
+            Console.WriteLine(File.Open(TestUtil.GetTestDataDir() + "test.txt", FileMode.Open, FileAccess.Read));
             Console.WriteLine("Test running");
             Assert.AreEqual(1, 1);
         }
---
+++ BrutePack-Tests/TestUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BrutePack_Tests
+{
+    [Flags]
+    public enum TestPathType
+    {
+        Root = 0,
+        MethodName = 1,
+        ClassName = 2,
+        MethodAndClassName = 3
+    }
+
+    public static class TestUtil
+    {
+        public static string GetTestDataDir(TestPathType pathType = TestPathType.MethodAndClassName)
+        {
+            var result = TestContext.CurrentContext.TestDirectory + "/../../TestData/";
+            if (pathType.HasFlag(TestPathType.ClassName))
+            {
+                var className = TestContext.CurrentContext.Test.ClassName;
+                result += className.Substring(className.LastIndexOf('.') + 1) + "/";
+            }
+            if (pathType.HasFlag(TestPathType.MethodName))
+                result += TestContext.CurrentContext.Test.MethodName + "/";
+            return result;
+        }
+    }
+}
