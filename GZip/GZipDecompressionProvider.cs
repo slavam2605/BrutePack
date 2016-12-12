3a89d0617b4b907cafcdc2e4eaf16309c8605de7
slava <slavam2605@bk.ru>
2016-12-12 21:10:05 +0300

    GZipDecompressionProvider implemented

---
+++ GZip/GZipDecompressionProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using BrutePack.Decompression;
+using BrutePack.FileFormat;
+
+namespace BrutePack.GZip
+{
+    public class GZipDecompressionProvider : IDecompressionProvider
+    {
+        [DecompressionProvider(BlockType.GZip)]
+        public byte[] Decompress(BrutePackBlock block)
+        {
+            var input = new MemoryStream(block.BlockData, 0, block.BlockData.Length);
+            var output = new MemoryStream();
+            GZipDecompressor.Decompress(input, output);
+            return output.ToArray();
+        }
+    }
+}
